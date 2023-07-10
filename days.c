3fcd710a882bdd591cd8720110b95dacd053adf8
temitope299 <tthriftwithus@gmail.com>
2023-07-10 07:56:53 -0700

    commit

---
+++ days.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void){
+  char day;
+  day = "tuesday";
+
+  switch(days){
+  case1 "monday":
+    printf("today is monday");
+  case2 "tuesday":
+    printf("today is tuesday");
+  case3 "wednesday":
+    printf("today is wednesday");
+  case4 "thursday":
+    printf("today is thursday");
+  default:
+    printf("you entered an invalid input");
+  }
+
+  
+ }
